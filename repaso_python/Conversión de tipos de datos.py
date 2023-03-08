f1cc2d9e6a6b4afc35040851c387ce974bc5c5b9
Fvaans <franciscocy1104@gmail.com>
2023-03-08 17:27:30 -0500

    Se agrega conversion de tipos de datos

---
+++ repaso_python/Conversión de tipos de datos.py
@@ -0,0 +1,26 @@
+#Conversión de cadenas a número
+
+cadena = "10"
+entero = int(cadena)
+decimal = float(cadena)
+
+#Conversión de números a cadenas
+
+entero = 10
+cadena = str(entero)
+
+#Conversión de números a booleanos
+
+numero = 10
+booleano = bool(numero)
+
+#Conversión de booleanos a números
+
+booleano = True
+numero = int(booleano)
+
+#Conversión de listas a tuplas
+
+lista = [1, 2, 3]
+tupla = tuple(lista)
+
