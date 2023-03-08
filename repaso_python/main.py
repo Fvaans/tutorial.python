7beb33430708bd1ba1ab7291eff5ae1adfcd7014
Fvaans <franciscocy1104@gmail.com>
2023-03-08 16:45:27 -0500

    Se agregan funciones

--- repaso_python/main.py
+++ repaso_python/main.py
@@ -1,13 +1,13 @@
-#Tipos de datos primitivos
-#Numeros enteros
+# Tipos de datos primitivos
+# Numeros enteros
 
-numero1: int =57
+numero1: int = 57
 print(numero1)
 
 numero2 = 24
 print(numero2)
 
-#Numeros reales
+# Numeros reales
 
 numero3: float = 4.9
 print(numero3)
@@ -21,13 +21,13 @@ esColombiano: bool = True
 esArgentino = False
 print(esColombiano)
 
-#Caracter y cadena de caracteres
+# Caracter y cadena de caracteres
 
 mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
 print(mensaje)
 
-#Operadores
-#Aritmeticos
+# Operadores
+# Aritmeticos
 
 numero5 = 9
 numero6 = 12
@@ -42,34 +42,52 @@ print("La multiplacion es=", multiplacion)
 print("La division es=", division)
 print("El modulo es=", modulo)
 
-#Asignación
+# Asignación
 x = 7
 y = 8
 z = 2
 print(x)
 
-#Logicos
-#and(y)
+# Logicos
+# and(y)
 
-q=5
+q = 5
 print(q > 4 and q < 9)
 
-#or (o)
+# or (o)
 p = 4
 print(p > 5 or p < 10)
 
-#not
-print(not(p > 2 and q < 7))
+# not
+print(not (p > 2 and q < 7))
 
-#Relacionales
+# Relacionales
 
 valor1 = 7
 valor2 = 9
 
-print(valor1 == valor2) #Igualdad
-print(valor1 > valor2) #Mayor que
-print(valor1 < valor2) #Menor que
-print(valor1 >= valor2) #Mayor igual que
-print(valor1 <= valor2) #Menor igual que
-print(valor1 != valor2) #Diferente
+print(valor1 == valor2)  # Igualdad
+print(valor1 > valor2)  # Mayor que
+print(valor1 < valor2)  # Menor que
+print(valor1 >= valor2)  # Mayor igual que
+print(valor1 <= valor2)  # Menor igual que
+print(valor1 != valor2)  # Diferente
+
+# Funciones
+"""
+ LAS FUNCIONES SON UN BLOQUE DE CODIGO QUE SOLO SE EJECUTAN CUANDO SE LLAMAN.
+"""
+
+
+def my_function():
+    print("¡Feliz dia!")
+
+
+my_function() # Invocar la funcion
+
+def message(name, lastname):
+    print("¡Feliz dia!" + name + " " +lastname)
+
+
+message("Sara", "Burbano")
 
